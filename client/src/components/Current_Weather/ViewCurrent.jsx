1d9b4d5e6ae4307deaac2526db9ae814d7835fd3
Alliaster Wentzel <alliasterw30@gmail.com>
2026-02-02 20:30:19 +0200

    added a file

--- client/src/components/Current_Weather/ViewCurrent.jsx
+++ client/src/components/Current_Weather/ViewCurrent.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-
-const box = {
-    display: "flex", 
-    justifyContent: "space-between", 
-    alignItems: "center", 
-    backgroundImage: "url('/images/bg-today-large.svg')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    borderRadius: "15px",
-    padding: "20px 50px",
-    maxWidth: "100%",
-    height: "200px",    
-    margin: "0 20px 0 0",  
-}
+import "./CurrentView.css"
 
 function CurrentView(props) {
     return (
-        <div style={box}>
+        <div className="box">
             <div>
                 <p style={{fontSize: "2rem", fontWeight: "bold"}}>{props.placeName}</p> 
                 <p>{props.date}</p>
